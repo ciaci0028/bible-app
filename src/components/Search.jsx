1a9844b54535d43e33e956151db6e362b11230c5
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-04-29 09:17:54 -0500

    created basic search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Search = () => {
+    
+    return (
+        <div>
+            <input placeholder="Search verse or keyword here"></input>
+            <button>Search</button>
+        </div>
+
+    )
+}
+
+export default Search
