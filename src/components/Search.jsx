97d3e53ad33ef766d07f8a44cce738dfae6f8940
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-04-29 09:29:31 -0500

    styling for search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import '../App.css';
 
 const Search = () => {
+
+    const [input, setInput] = React.useState('');
     
     return (
-        <div>
-            <input placeholder="Search verse or keyword here"></input>
-            <button>Search</button>
+        <div className='container'>
+            <input 
+                className='input'
+                placeholder="Search verse or keyword here" 
+                value={input}
+                onChange={event => setInput(event.target.value)}
+                >
+                </input>
+            <button
+                className='button'
+            >
+                Search
+            </button>
         </div>
 
     )
